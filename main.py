d94c8e411a0f92691d32dda39e95f367d8c226c8
Lavanya van Wyk <lavanyavanwyk1@gmail.com>
2025-07-02 13:44:23 +0200

    Added plot method using dictionary from csv file
    Plotted population data by year using data read from population_per_continent dictionary created using data from csv. Update csv read method to store years and population data as integers. Added labels and header for data visual.

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import csv
 import matplotlib.pyplot as plt
 
-population_per_continent = {}
+
+population_per_continent = {
+}  #init empty dictionary to populate with data read from csv
 
 with open('data.csv', mode='r') as file:
   csvFile = csv.DictReader(file)
   for lines in csvFile:
 
     continent = lines['continent']
-    year = lines['year']
-    population = lines['population']
+    year = int(lines['year'])
+    population = int(lines['population'])
 
     if continent not in population_per_continent:
-      population_per_continent[continent] = {'population': [], 'years': []}
+      population_per_continent[continent] = {
+          'population': [],
+          'years': []
+      }  #creates dictionary with key-values pairs containing lists
 
-    population_per_continent[continent]['population'].append(population)
-    population_per_continent[continent]['years'].append(year)
+    population_per_continent[continent]['population'].append(
+        population)  #adds population data into the 'population' value list
+    population_per_continent[continent]['years'].append(
+        year)  #adds year data into the 'years' value list
 
-print(population_per_continent)
+  for continent in population_per_continent:
+    population = population_per_continent[continent][
+        'population']  # loops through main_dict[continent key]['population' key]
+    years = population_per_continent[continent][
+        'years']  #loops through main_dict[continent key]['years' key]
+    plt.plot(years, population, label=continent, marker=".", alpha=0.5)
+
+plt.title("Internet Population per continent")
+plt.xlabel("Year")
+plt.ylabel("Internet users (in millions)")
+plt.grid(True)
+plt.show()
