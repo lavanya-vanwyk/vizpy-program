7d07285cf8de4b9451bf61444ad6051db1874426
Lavanya van Wyk <lavanyavanwyk1@gmail.com>
2025-07-02 10:26:45 +0200

    Added csv file and csv reading test method
    Added csv file containing data obtained from "ourworldindata.org". Added dictReader method with variables assigned to keys in dictionaries, to test parse and print.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+import csv
+
+with open('data.csv', mode='r') as file:
+  csvFile = csv.DictReader(file)
+  for lines in csvFile:
+
+    continent = lines['continent']
+    year = lines['year']
+    population = lines['population']
+
+    print(f'The population in {continent} in the year {year}, was {population} people!')
