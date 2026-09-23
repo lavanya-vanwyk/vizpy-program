0b2302bdbead2295289fde984464144e81f7c0b9
Lavanya <lavanjhb025@student.wethinkcode.co.za>
2026-09-23 10:20:03 +0200

    chore: clean up outdated initial matplot visualisation code files causing pipeline failure

--- main.py
+++
@@ -1,9 +0,0 @@
-from create_graph import CreateGraph
-
-new_graph = CreateGraph()
-
-data_filename = 'data.csv'
-
-new_graph_data = new_graph.generate_population_dictionary(data_filename)
-
-new_graph.display_graph(new_graph_data)
